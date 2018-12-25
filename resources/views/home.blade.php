2495779d331881205f9d1159d3094659a43f1d08
Vlaqwrqcwzx <tapok-vova98@mail.ru>
2018-12-25 15:34:19 +0300

    add laratrust in blade template

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,9 +5,11 @@
 @section('content')
 <div class="list-group">
   <a class="list-group-item list-group-item-action active" href="#list-home">Главная</a>
-  <?php if (Auth::user()->hasRole('admin')) {?>
+
+@role('admin')
   <a class="list-group-item list-group-item-action"  href="/users">Все пользователи</a>
   <a class="list-group-item list-group-item-action"  href="/users/create">Создать пользователя</a>
-  <?}?>
+@endrole
+
 </div>
 @stop
